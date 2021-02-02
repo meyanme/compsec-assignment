714930836b0f40744b3833b2f39900c10ba349ee
Syamil Anuar <ahmsyamil@gmail.com>
2021-02-03 02:46:28 +0800

    commit #3: added if option in main()

--- caesar.py
+++ caesar.py
@@ -5,14 +5,21 @@ Created on Wed Dec 23 11:59:50 2020
 @author: Computer Security (TSN3251)  Trimester 2022
 """
 #https://www.tutorialspoint.com/cryptography_with_python/cryptography_with_python_caesar_cipher.htm
+import os
+import sys
+
+
 def openFile(textfile):
    file_content = ""
    file_name = textfile+'.txt'
-   with open(file_name, 'r+') as file:
-      line_in_file = file.readlines()
-      for line in line_in_file:
-         file_content+=line
-   return file_content
+   if (os.path.exists(file_name)):
+      with open(file_name, 'r+') as file:
+         try:
+            line_in_file = file.readlines()
+            for line in line_in_file:
+               file_content+=line
+            return file_content
+         except:(FileNotFoundError)
 
 
 def encrypt(text,s):
@@ -55,36 +62,53 @@ def hackcaeser(message,LETTERS):
 
 
 def main():
-
-   print ("#1: Encrypt from file\n")
-   print ("#2: Decrypt from file\n")
-   print ("#3: Hack cipher with brute force\n")
-   option = input("Option = ")
-   if option == 1:
-
-   elif option == 2:
+   while True:
+      print ("#1: Encrypt from file")
+      print ("#2: Decrypt from file")
+      print ("#3: Hack cipher with brute force")
+      print ("#4: Exit")
       
-   elif option == 3:
+      option = input("\tOption = ")
+      print()
+   
+      if option == "1":
+         filename = input('filename:\n')
+         plaintext = openFile(filename)
+         print ("File content: ", plaintext)
+         #plaintext = input('enter message:\n')
+         key = int(input('enter your key:\n'))
+         ciphertext = encrypt(plaintext, key)
+         print ('\n')
+         main()
 
-   else:
+      elif option == "2":
+         pass
+         #TODO: DECRYPTFUNCTION
 
+      elif option == "3":
+         pass
+         print ("Plaintext : " + plaintext)
+         print ("Shift pattern : " + str(key))
+         print ("Ciphertext: ",  ciphertext)
 
-   filename = input('filename:\n')
-   plaintext = openFile(filename)
-   #plaintext = input('enter message:\n')
-   key = int(input('enter your key:\n'))
-   ciphertext = encrypt(plaintext, key)
-   print ('\n')
+         message = ciphertext                        #encrypted message
+         LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+         hackcaeser(message,LETTERS)
+
+      elif option == "4": 
+         break
+
+      else:
+         print ("Invalid Input\n\n")
+         continue
+     
+   
+
+   
    #text = "CEASER CIPHER DEMO"
    #s = 4
 
-   print ("Plaintext : " + plaintext)
-   print ("Shift pattern : " + str(key))
-   print ("Ciphertext: ",  ciphertext)
-
-   message = ciphertext                        #encrypted message
-   LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-   hackcaeser(message,LETTERS)
+   
 
 
 # Executes the main function
