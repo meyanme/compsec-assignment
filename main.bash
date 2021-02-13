c59b0a75c33e694c96b52f1fbd0b341fef7be271
Syamil Anuar <ahmsyamil@gmail.com>
2021-02-14 00:56:36 +0800

    added comment

--- main.bash
+++ main.bash
@@ -13,29 +13,25 @@ main(){
 
 		case $opt in
 			Encryption)
-			#encryption timeee
 			encrypt
 			echo ''
 			;;
 
 			Decryption)
-			#decryption timeee
 			decrypt
 			echo ''
 
 			;;
 
 			Hack)
-			#hacking timeee
 			hacking
 			echo ''
 			;;
 
 			Exit)
-			#exit the program
 			echo "Exit Succesfully"
 			exit                    
-		esac                        #close case
+		esac                        
 	done
 }
 
@@ -43,31 +39,31 @@ encrypt(){
 	echo '             E N C R Y P T I O N             '
 	echo '---------------------------------------------'
 	newcharacter=''
-	input_encrypt
+	input_encrypt									#function to read file
 	plainText=$text
 	key=$encryptkey
 
 	echo 'Received Plain Text is --> ' ${plainText}
 	echo 'Received key is        --> ' ${key}
-	length=${#plainText}            # = length
-	for ((i=0; i<length; i++))      # for do loop ada 2 (())
+	length=${#plainText}            				#length of plaintext
+	for ((i=0; i<length; i++))      				#iterate each character in plaintext
 	do
-		character=${plainText:i:1}                  #content in index (i:1 = i iterate one)
-		testing=$(printf "%d" "'${character}")      #index array
-		if [[ ${character} =~ [A-Z] ]]              # =~ --> content is in array/etc
+		character=${plainText:i:1}                  #split into single char
+		testing=$(printf "%d" "'${character}")      #convert to ASCII
+		if [[ ${character} =~ [A-Z] ]]              #check for uppercase
 	       	then
-			let testing=$(($testing+key-65))        # let is need in math operation       
+			let testing=$(($testing+key-65))        #ceaser calculation
 			let testing=$(($testing%26+65))
-		elif [[ ${character} == ' ' ]]
+		elif [[ ${character} == ' ' ]]				#check for whitespaces
 			then
 			let testing=90
 		else
 			let testing=$(($testing+key-97))
 			let testing=$(($testing%26+97))
-		fi                                          #tutup if
-		newcharacter=$newcharacter$(printf "\x$(printf %x $testing)")
+		fi                                         
+		newcharacter=$newcharacter$(printf "\x$(printf %x $testing)")	#append encrypted result
 		echo ${newcharacter}
-	done                                            #tutup for
+	done                                           
 	echo "Encryption Done"
 }
 
@@ -75,20 +71,20 @@ decrypt(){
 	echo '             D E C R Y P T I O N             '
 	echo '---------------------------------------------'
 	newcharacter=''
-	input_decrypt
+	input_decrypt								#function to read file	
 	cipherText=$text
 	key=$encryptkey
 
 	echo 'Received Cipher Text is --> ' ${cipherText}
 	echo 'Received key is         --> ' ${key}
-    length=${#cipherText}
-	for ((i=0; i<length; i++))
+    length=${#cipherText}						#ciphertextlength
+	for ((i=0; i<length; i++))					#iterate text length
 	do 
-		character=${cipherText:i:1}
-		deci=$(printf "%d" "'${character}")
-		if [[ ${character} =~ [A-Z] ]]
+		character=${cipherText:i:1}				#split intro single char
+		deci=$(printf "%d" "'${character}")		#convert to ASCII
+		if [[ ${character} =~ [A-Z] ]]			#check for uppercase
 		then
-			let deci=$(($deci-key-65))
+			let deci=$(($deci-key-65))			#ceaser calculation
 			if [[ ${deci} -lt 0 ]]
 			then
 				let deci=$(($deci%26+26+65))
@@ -105,7 +101,7 @@ decrypt(){
 				let deci=$(($deci%26+97))
 			fi
 		fi
-		newcharacter=${newcharacter}$(printf "\x$(printf %x $deci)")
+		newcharacter=${newcharacter}$(printf "\x$(printf %x $deci)") #append decrypted result
 		echo ${newcharacter}
 	done
 	echo "Decryption Done"
@@ -116,94 +112,94 @@ hacking() {
 	echo '    B R U T E   F O R C E  H A C K I N G     '
 	echo '---------------------------------------------'
     
+	#array of 26 alphabet for brute force checking
     LETTERS=('A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z')
     letters=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
  
-    #read -p 'Enter Ciphertext: ' cipherText
-	input_file
+	input_file								#function to read file
 	cipherText=$text
     echo 'Received Cipher Text is --> ' ${cipherText}
     echo ''
-    cipherLength=${#cipherText}
-    length=${#LETTERS[@]}
+    cipherLength=${#cipherText}				#text length
+    length=${#LETTERS[@]}					#length 26 to try all 26 keys
        
-    for ((key=0; key<length; key++))                    #for loop to iterate [A-Z]
+    for ((key=0; key<length; key++))        #loop for 26 keys
     do
         translated="->"
         num=0
-        for ((i=0; i<cipherLength; i++))   #for loop to iterate cipherText
+        for ((i=0; i<cipherLength; i++))    #loop for ciphertext
         do
-            cipherChar=${cipherText:i:1}
+            cipherChar=${cipherText:i:1}	#split text into single char
             count=0
-            for j in {A..Z}
+            for j in {A..Z}					#loop to find decrypted char value
             do
-                if [[ $j == $cipherChar ]]
+                if [[ $j == $cipherChar ]]	#if value found, exit
                     then
                     break
                 fi
-                let count=$count+1
+                let count=$count+1			#increment value until found
             done
         
-            if grep -q ${cipherChar} <<< ${LETTERS[@]}       #check upper()
+            if grep -q ${cipherChar} <<< ${LETTERS[@]}      #check uppercase
                 then
                 let num=$count
-                let num=$num-$key
+                let num=$num-$key							#decrypted char value minus attempted key(0-25)
                 if [[ $num<0 ]]
                     then
                     let num=$num+26
                 fi
                 translated=${translated}${LETTERS[$num]}
                 
-            elif grep -q ${cipherChar} <<< ${letters[@]}     #check lower()
+            elif grep -q ${cipherChar} <<< ${letters[@]}    #check lowercase
                 then
                 let num=$count
-                let num=$num-$key
+                let num=$num-$key							#decrypted char value minus attempted key(0-25)
                 if [[ $num<0 ]]
                     then
                     let num=$num+26
                 fi
-                translated=${translated}{$letters[$num]}
+                translated=${translated}{$letters[$num]}	#append brute force hack result
                 
             else
                 translated=${translated}${cipherChar}
             fi
         done
-        echo 'Hacking Key #'$key': '$translated
+        echo 'Hacking Key #'$key': '$translated				#print the result per key
     done
 }
 
 
 input_decrypt(){
 	text=''
-	read -p 'Enter filename :' filename
-	read -p 'Key            :' encryptkey
+	read -p 'Enter filename :' filename			#get file name
+	read -p 'Key            :' encryptkey		#get decryption key
 	txt='.txt'
 	filename=$filename$txt
 	echo 'File ('$filename') is being read'
 	echo ''
-	text=$(<$filename)
+	text=$(<$filename)							#read file content
 }
 
 
 input_encrypt(){
 	text=''
-	read -p 'Enter filename :' filename
-	read -p 'Key            :' encryptkey
+	read -p 'Enter filename :' filename			#get file name
+	read -p 'Key            :' encryptkey		#get decryption key
 	txt='.txt'
 	filename=$filename$txt
 	echo 'File ('$filename') is being read'
 	echo ''
-	text=$(<$filename)
+	text=$(<$filename)							#read file content
 }
 
 input_file() {
 	text=''
-	read -p 'Enter filename :' filename
+	read -p 'Enter filename :' filename			#get file name
 	txt='.txt'
 	filename=$filename$txt
 	echo 'File ('$filename') is being read'
 	echo ''
-	text=$(<$filename)
+	text=$(<$filename)							#read file content
 
 }
 
